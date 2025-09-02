cf8e877d4f0fbc9c5fbd5e477367e1b7c7ab3e8e
Sabbaghtiago <sabbaghtiago2@gmail.com>
2025-09-02 16:52:04 -0300

    1.3 falta verificacion de datos

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ matriz = [
 def main():
     print('Bienvenido a schipani motors sport esta es nuestra disponibilidad ')
     funcion.mostrar_matriz(matriz)
+    #Alejo va a hacer la verificacion de datos
+    print('Seleccione la marca que le gustaria ver')
+    marca = int(input('ingrese 1 para Toyota, 2 para Honda, 3 para Chevrolet y 4 para Ford '))
+    print('ahora seleccione los modelos que quiere ver')
+    modelo = int(input('Ingrese 1 para Hatchback, 2 para Sedan, 3 para Suv y 4 para PickUp '))
+    
+
 
 main()
     
