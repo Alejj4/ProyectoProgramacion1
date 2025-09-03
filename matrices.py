b234c0ed937ad66f754ce3cf473f1807651cf128
Gonzalorag <graggio.ar@gmail.com>
2025-09-03 14:41:13 -0300

    relleno parcial de las matricitas

--- matrices.py
+++ matrices.py
@@ -2,21 +2,25 @@
 
 toyota_hatchback = [[1,2,3],
                     [ 28,31,33]]  #xs xls s
-toyota_sedan = [[]]
-toyota_suv =[[]]
-toyota_pickup =[[]]
-honda_hatchback = [[]]
-honda_sedan = [[]]
-honda_suv=[[]]
+toyota_sedan = [[0]]
+toyota_suv =[[1,3],  #xli, seg
+             [44,52]]
+toyota_pickup =[[1,3],   #dx4x4, sr4x4
+                [53,61]]
+honda_hatchback = [[0]]
+honda_sedan = [[3], #honda civic
+               [28]] 
+honda_suv=[[2,3],   #hr-v, cr-v
+           [25,30]]
 honda_pickup=[[]]
-chevrolet_hatchback = [[]]
+chevrolet_hatchback = [[0]]
 chevrolet_sedan = [[]]
 chevrolet_suv = [[]]
 chevrolet_pickup = [[]]
 ford_hatchback = [[]]
 ford_sedan = [[]]
 ford_suv =[[]]
-ford_pickup =[[]]
+ford_pickup =[[0]]
 
 Matriz_principal =[[toyota_hatchback,toyota_sedan,toyota_suv,toyota_pickup],
                    [honda_hatchback, honda_sedan, honda_suv, honda_pickup],
