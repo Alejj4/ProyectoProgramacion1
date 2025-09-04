41267b0984b7744fead46532379d160004ba95c3
lautaro165 <lautaro25@outlook.com.ar>
2025-09-04 17:45:31 -0300

    1.10.1 Modifiqué cómo se ven las matrices de autos sin stock

--- matrices.py
+++ matrices.py
@@ -5,37 +5,37 @@ import funcion as fn
 def llamar_matriz(fila,columna):
     toyota_hatchback = [[1,2,3],
                         [ 28,31,33]]  #yaris-xs, yaris-xls yaris-s
-    toyota_sedan = [[0]]
+    toyota_sedan = []
     toyota_suv =[[1,3],  #cross xli, cross seg
                 [44,52]]
     toyota_pickup =[[1,3],   #hilux dx4x4, hiluxsr4x4
                     [53,61]]
-    schipani_hatchback = [[0]]
+    schipani_hatchback = []
     schipani_sedan = [[1,2], #schipani uade Seminuevo, Schipani Schiziano
                 [28,35]] 
     schipani_suv=[[2,3],   #schipani chipa, schipani bochoQuemado
             [25,30]]
     schipani_pickup=[[3],   #Schipani Pipani
                 [48]]
-    chevrolet_hatchback = [[0]]
+    chevrolet_hatchback = []
     chevrolet_sedan = [[2], #onixplus
                     [27]]
     chevrolet_suv = [[1,2],  #spring, tracker
                     [31,32]]
     chevrolet_pickup = [[1,2,3],  #montana, S10, silverado
                         [34,43,102]]
-    ford_hatchback = [[0]]
-    ford_sedan = [[0]]
+    ford_hatchback = []
+    ford_sedan = []
     ford_suv =[[2,3,3],   # territory, kuga hibrido,  Everest
             [43,76,85]]
     ford_pickup =[[2,3,3],      # maverick, ranger raptor, f150 lariat hibrido
                 [51,116, 124]]
         #Gran matriz con todas las submatrices
-    Matriz_principal =[[toyota_hatchback,toyota_sedan,toyota_suv,toyota_pickup],
+    matriz_principal =[[toyota_hatchback,toyota_sedan,toyota_suv,toyota_pickup],
                     [schipani_hatchback, schipani_sedan, schipani_suv, schipani_pickup],
                     [chevrolet_hatchback, chevrolet_sedan, chevrolet_suv, chevrolet_pickup],
                     [ford_hatchback, ford_sedan, ford_suv, ford_pickup]]
-    return Matriz_principal[fila][columna]
+    return matriz_principal[fila - 1][columna - 1]
 
 def matriz_compra(fila, columna,matriz):
     # llama la matriz compravehiculos que la arrastre y suma 1 cuando hay una venta en base a lo ingresado por el usuario
