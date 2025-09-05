5dae4d11bb648b082b444b6333fb13314c3e230a
tizisch <tizischipani07@gmail.com>
2025-09-05 13:41:31 -0300

    random

--- funcion.py
+++ funcion.py
@@ -1,4 +1,5 @@
 import matrices as mt
+import random as rn
 
 def mostrar_matriz(matriz):
     esquina = 'Marcas/Autos'
@@ -118,3 +119,32 @@ def verificar_modelo():
         print("Opción incorrecta! Intente de nuevo.")
         modelo = int(input('Ingrese 1 para Hatchback, 2 para Sedan, 3 para Suv y 4 para PickUp: '))
     return modelo
+
+import random as rn 
+def descuento_auto():
+       descuento = 0
+       des = int(input("¿Desea participar de un juego para conseguir un descuento del 20% para la compra de su auto? Ingrese 1 si quiere y 2 si no quiere. "))
+       while des != 1 and des != 2:
+              des = int(input("Su respuesta es incorrecta. Ingrese 1 si quiere participar y 2 si no quiere. "))
+       if des == 2: 
+              print("Como usted desee, aqui tiene la lista de autos con los que contamos")
+       else: 
+              print("Nos alegra que haya querido participar. El juego trata de que tiene que elegir un numero del 1 al 5, si su numero es igual al que eligio el programa usted se gana el descuento asi de facil.")
+              ran = rn.randint(1,5)
+              numran = int(input("Ingrese un numero del 1 al 5: "))
+              while numran < 1 or numran > 5:
+                     numran = int(input("Su numero es incorrecto, ingrese un numero entre 1 y 5: "))
+              if numran != ran:
+                    print("Lo lamentamos, pero su numero no coincide, el numero correcto era:", ran)
+              else:
+                    print("¡Felicitaciones! Su numero coicide, usted se gano un descuento del 20%.")
+                    descuento += 1
+       return descuento
+
+
+
+
+
+
+
+
--- pruebas.py
+++ pruebas.py
@@ -1,24 +1,21 @@
-# Archivo de pruebas para experimentar fuera de los archivos principales
-# Borrar cuando el proyecto este terminado
-import matrices as mt
+import random as rn 
+def descuento_auto():
+       descuento = 0
+       des = int(input("¿Desea participar de un juego para conseguir un descuento del 20% para la compra de su auto? Ingrese 1 si quiere y 2 si no quiere. "))
+       while des != 1 and des != 2:
+              des = int(input("Su respuesta es incorrecta. Ingrese 1 si quiere participar y 2 si no quiere. "))
+       if des == 2: 
+              print("Como usted desee, aqui tiene la lista de autos con los que contamos")
+       else: 
+              print("Nos alegra que haya querido participar. El juego trata de que tiene que elegir un numero del 1 al 5, si su numero es igual al que eligio el programa usted se gana el descuento asi de facil.")
+              ran = rn.randint(1,5)
+              numran = int(input("Ingrese un numero del 1 al 5: "))
+              while numran < 1 or numran > 5:
+                     numran = int(input("Su numero es incorrecto, ingrese un numero entre 1 y 5: "))
+              if numran != ran:
+                    print("Lo lamentamos, pero su numero no coincide, el numero correcto era:", ran)
+              else:
+                    print("¡Felicitaciones! Su numero coicide, usted se gano un descuento del 20%.")
+                    descuento += 1
+       return descuento
 
-matriz = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,4,5],
-    [3,5,6,8]
-    ]
-
-
-def precios(matriz):
-       for columna in range(4):
-              for fila in range(4):
-                     promedio_autos = mt.llamar_matriz(columna ,fila)
-                     if len(promedio_autos)>1:
-                            preciospromedios=sum(promedio_autos[1])//len(promedio_autos[1])
-                     else:
-                            preciospromedios = 0
-                     matriz[columna][fila]=preciospromedios
-       return matriz
-
-print(precios(matriz))
